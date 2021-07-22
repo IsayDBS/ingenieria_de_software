2923da66b9de91a50746b67213321f6506fa470d
Toporaku <toporaku@etopo-Legion5.localdomain>
2021-07-22 05:29:19 -0500

    Set up inicial del proyecto

---
+++ src/main/java/is/pims/MercadoManazo/MercadoManazoApplication.java
@@ -0,0 +1,13 @@
+package is.pims.MercadoManazo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MercadoManazoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MercadoManazoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/is/pims/MercadoManazo/MercadoManazoApplicationTests.java
@@ -0,0 +1,13 @@
+package is.pims.MercadoManazo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MercadoManazoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
