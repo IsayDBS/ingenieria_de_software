d839a89e6fd4bf55b81d8f54c350b5581c696b86
Eric Toporek <toporek.inc@ciencias.unam.mx>
2021-08-06 04:01:19 -0500

    Primer indicio de registro, modificando estructura a usuario.

---
+++ src/main/java/is/pims/MercadoManazo/controller/RegistroUsuarioController.java
@@ -0,0 +1,20 @@
+package is.pims.MercadoManazo.controller;
+
+import is.pims.MercadoManazo.dto.SolicitudRegistro;
+import is.pims.MercadoManazo.service.RegistroService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "register")
+@AllArgsConstructor
+public class RegistroUsuarioController {
+
+    private final RegistroService registroService;
+
+    public String registro(@RequestBody SolicitudRegistro req){
+        return registroService.registro(req);
+    }
+}
---
+++ src/main/java/is/pims/MercadoManazo/repository/UsuarioRepository.java
@@ -0,0 +1,15 @@
+package is.pims.MercadoManazo.repository;
+
+import is.pims.MercadoManazo.dto.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Repository
+@Transactional(readOnly = true)
+public interface UsuarioRepository extends JpaRepository<Usuario,Long> {
+
+    Optional<Usuario> encuentraPorCorreo(String mail);
+}
