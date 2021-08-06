727f7bdcd3ecc0c681e2e6a6b924c7c680029ae4
Eric Toporek <toporek.inc@ciencias.unam.mx>
2021-08-06 07:30:31 -0500

    Guardado de servicio en la base de datos.Generación de una contraseña aleatoria y hashing.

--- src/main/java/is/pims/MercadoManazo/service/RegistroService.java
+++ src/main/java/is/pims/MercadoManazo/service/RegistroService.java
@@ -1,12 +1,32 @@
 package is.pims.MercadoManazo.service;
 
+import is.pims.MercadoManazo.dto.RolUsuario;
 import is.pims.MercadoManazo.dto.SolicitudRegistro;
+import is.pims.MercadoManazo.dto.Usuario;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class RegistroService {
 
+    private final UsuarioService usuarioService;
+    private final EmailValidator emailValidator;
+
     public String registro(SolicitudRegistro req){
-        return "good";
+        boolean isValid = emailValidator.test(req.getCorreo());
+        if (!isValid)
+            throw new IllegalStateException("Correo no válido");
+
+        return usuarioService.registraUsuario(
+                new Usuario(
+                        req.getNombre(),
+                        req.getApellido(),
+                        req.getCorreo(),
+                        req.getTelefono(),
+                        RolUsuario.COMPRADOR
+                ),
+                RolUsuario.COMPRADOR
+        );
     }
 }
