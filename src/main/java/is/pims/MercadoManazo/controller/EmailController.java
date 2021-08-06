400151f8db405a71aa51b7a0cc4cd48758f13d75
Isay <isaydamar@ciencias.unam.mx>
2021-08-05 23:01:05 -0500

    Actualizacion de rutas

--- src/main/java/is/pims/MercadoManazo/controller/EmailController.java
+++ src/main/java/is/pims/MercadoManazo/controller/EmailController.java
@@ -1,9 +1,11 @@
 package is.pims.MercadoManazo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +13,7 @@ import is.pims.MercadoManazo.dto.Email;
 import is.pims.MercadoManazo.service.EmailService;
 
 @RestController
+@CrossOrigin(origins = "*", methods= {RequestMethod.GET,RequestMethod.POST})
 @RequestMapping(value="/email")
 public class EmailController {
 		
--- src/main/java/is/pims/MercadoManazo/controller/ProductoController.java
+++ src/main/java/is/pims/MercadoManazo/controller/ProductoController.java
@@ -3,18 +3,21 @@ package is.pims.MercadoManazo.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import is.pims.MercadoManazo.dto.Producto;
 import is.pims.MercadoManazo.service.ProductoService;
 
 @RestController
+@CrossOrigin(origins = "*", methods= {RequestMethod.GET,RequestMethod.POST,RequestMethod.DELETE,RequestMethod.PUT})
 @RequestMapping("/producto")
 public class ProductoController {
 
